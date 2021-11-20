4c66c0812b6116c54ee8dbeadaa5cb8356c1f2f3
Pawel <javorski.pavel@gmail.com>
2021-11-20 23:57:13 +0100

    bycrypt, express

--- app/app.js
+++ app/app.js
@@ -3,5 +3,8 @@ const path = require('path');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
+app.use('/api', require('./routes/api'));
+app.use(require('./routes/web'));
+
 
 module.exports = app;
